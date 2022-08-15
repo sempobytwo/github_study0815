dfe205ba1fda56a6b8d1ab09ae3a80828ddfe61d
sempobytwo <111268677+sempobytwo@users.noreply.github.com>
2022-08-15 16:33:17 +0900

    for  반복문추가

--- study01.py
+++ study01.py
@@ -6,8 +6,12 @@ if a>5:
 else:
 	print('작다')	
 
+for i in range(10):
+	if a % i ==0:
+		print(i)
+		
 # 1 로컬에 github_study0815 폴더생성
 # 2 github_study0815  폴더에 study01.py라는 파일 생성
 # 3 study01.py에 코드를 작성
-# 4 study01.py 코드 정상 실행 확인 ( 탐색기에서 shift + 마우스 오른쪽버튼)
+# 4 study01.py 코드 정상 실행 확인 ( 탐색기에서 shift + 마우스 오른쪽버튼: 파워셀실행됨)
 # 5로컬에 생성된 github_study0815  <-> Github저장소
